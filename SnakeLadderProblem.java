5775217375e9260745cbeccbb9d4bbecc56600e0
abhishekpachkawade <abhishekpachkawade@gmail.com>
2021-09-12 17:21:04 +0530

    Rolling a die to get random number between 1 to 6

--- SnakeLadderProblem.java
+++ SnakeLadderProblem.java
@@ -12,14 +12,14 @@ public class SnakeLadderProblem {
 		System.out.println("Wellcome to Snake and Ladder Game");
 		// Player position at start
 		int startPosition = 0;
-		//dice value is between 1 to 6
+		// dice value is between 1 to 6
 		int minDiceValue = 1;
 		int maxDiceValue = 6;
-		//nextInt is exclusive of top value so added plus 1
-		int randomNum = ThreadLocalRandom.current().nextInt(minDiceValue,maxDiceValue +1);
+		// nextInt is exclusive of top value so added plus 1
+		int randomNum = ThreadLocalRandom.current().nextInt(minDiceValue, maxDiceValue + 1);
 		System.out.println("The initial position of a player is : " + startPosition);
-        System.out.println("After rolling a die position of player is at : "+randomNum);
+		System.out.println("After rolling a die position of player is at : " + randomNum);
 
 	}
-	
+
 }
